585ad4c37ad1e8c3d0bb8dca03364749d45c91a4
tirthosarkar <tirthosarkar1@gmail.com>
2026-02-20 22:20:26 +0600

    Implement Login Logic And navigate to Homepage

--- script/login.js
+++ script/login.js
@@ -1,1 +1,24 @@
 console.log("Loging functionality is coming soon...");
+
+document.getElementById("login-btn").addEventListener("click", function() {
+   //1- get the mobile number input
+    const numberInput = document.getElementById("input-number");
+    const contactNumber = numberInput.value;
+    console.log(contactNumber);
+
+    //2- get the pin number input
+    const inputPin = document.getElementById("input-pin");
+    const pin = inputPin.value;
+    console.log(pin);
+
+    //3- march pin & mobile number
+        if (contactNumber == "01518750179" && pin == "1234"){
+
+        //     //4- if match show success alert
+            alert("Login successful!");
+            window.location.assign("home.html");
+        } else {
+        //     //5- if not match show error alert
+            alert("Invalid mobile number or pin. Please try again.");
+        }
+});
